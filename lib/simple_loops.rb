e31f9f0bcb4413a0deed943b8db9a0de5187a3f3
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-02 18:51:41 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  counter = 0
+  while counter < 5 do
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times(message, n)
+  counter = 0
+  while counter < n do
+    puts message
+    counter += 1
+  end
+end
+
+def output_array(array)
+  len = array.length
+  counter = 0
+  while counter < len do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  len = array.length
+  counter = 0
+  while counter < len do
+    array[counter] = array[counter].to_s
+    counter += 1
+  end
+  return array
+end
